24a2eb99827ee4285806d6557956d8c486f76567
BakHyeonJun <parkhj86@naver.com>
2025-08-21 11:46:14 +0900

    how to use commit

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -5,7 +5,15 @@ public class TestClass {
 	public static void main(String[] args) {
 	// 확정짓기 연습
 	
+		
+		}
+		
+	public void pull() {
+		System.out.println("Pull을 잘 하자.");
+		
+		}
+	public void commit() {
+		
 	}
-	
 	
 }
